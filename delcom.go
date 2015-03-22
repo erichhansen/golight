4583d8f34f384f3987b6aea89ef9eac18f27b336
Erich Hansen <et_hansen@yahoo.com>
2015-03-22 19:57:36 -0400

    adds flash

--- delcom.go
+++ delcom.go
@@ -12,6 +12,48 @@ import(
     var Red byte = 0x02
     var Yellow byte = 0x04
 
+func Flash(color byte){
+leds, err := hid.Open(vendorId, productId, "")
+    if err != nil {
+        log.Fatalf("Could not open leds device: %s", err)
+    }
+    log.Println("Opened connection")
+    defer leds.Close()
+
+    /*
+        Major Command 1 Byte
+        Minor Command 1 Byte
+        Data LSB      1 Byte
+        Data MSB      1 Byte
+        DataHID[0..3] 4 Bytes
+        DataExt[0..7] 8 Bytes (Optional)
+
+        Only 2 major commands, 101 (0x65) and 102 (0x66)
+        101 - sending 8 bytes
+        102 - sending 16 bytes
+
+    */
+    data := make([]byte, 8)
+    data[0] = 0x65   // Major command
+    data[1] = 0x14   // Minor command
+    data[2] = color  // Data LSB
+    data[3] = 0x00   // Data MSB
+    data[4] = 0x00   // DataHID[0]
+    data[5] = 0x00   // DataHID[1]
+    data[6] = 0x00   // DataHID[2]
+    data[7] = 0x00   // DataHID[3]
+
+    bytesWritten, err := leds.SendFeatureReport(data)
+    if err != nil {
+        log.Fatalf("Could not write bytes to delcom LED action. %s\n", err)
+    }
+
+    if bytesWritten == -1 {
+        log.Fatalf("No bytes written")
+    }
+}
+
+
 func On(color byte) {
     leds, err := hid.Open(vendorId, productId, "")
     if err != nil {
