8572acd40f26dab27ac9259eaa2fe55f27c50840
Erich Hansen <et_hansen@yahoo.com>
2015-03-22 20:33:49 -0400

    refactor and code cleanup

--- delcom.go
+++ delcom.go
@@ -12,95 +12,34 @@ import(
     var Red byte = 0x02
     var Yellow byte = 0x04
 
-func Flash(color byte){
-leds, err := hid.Open(vendorId, productId, "")
-    if err != nil {
-        log.Fatalf("Could not open leds device: %s", err)
-    }
-    log.Println("Opened connection")
-    defer leds.Close()
 
-    /*
-        Major Command 1 Byte
-        Minor Command 1 Byte
-        Data LSB      1 Byte
-        Data MSB      1 Byte
-        DataHID[0..3] 4 Bytes
-        DataExt[0..7] 8 Bytes (Optional)
-
-        Only 2 major commands, 101 (0x65) and 102 (0x66)
-        101 - sending 8 bytes
-        102 - sending 16 bytes
-
-    */
-    data := make([]byte, 8)
-    data[0] = 0x65   // Major command
-    data[1] = 0x14   // Minor command
-    data[2] = color  // Data LSB
-    data[3] = 0x00   // Data MSB
-    data[4] = 0x00   // DataHID[0]
-    data[5] = 0x00   // DataHID[1]
-    data[6] = 0x00   // DataHID[2]
-    data[7] = 0x00   // DataHID[3]
-
-    bytesWritten, err := leds.SendFeatureReport(data)
-    if err != nil {
-        log.Fatalf("Could not write bytes to delcom LED action. %s\n", err)
-    }
-
-    if bytesWritten == -1 {
-        log.Fatalf("No bytes written")
-    }
+type command struct {
+    color byte
+    flash bool
+    shutdown bool
 }
 
+func Flash(color byte){
+    sendCommand(command{color: color, flash: true})
+}
 
 func On(color byte) {
-    leds, err := hid.Open(vendorId, productId, "")
-    if err != nil {
-        log.Fatalf("Could not open leds device: %s", err)
-    }
-    log.Println("Opened connection")
-    defer leds.Close()
-
-    /*
-        Major Command 1 Byte
-        Minor Command 1 Byte
-        Data LSB      1 Byte
-        Data MSB      1 Byte
-        DataHID[0..3] 4 Bytes
-        DataExt[0..7] 8 Bytes (Optional)
-
-        Only 2 major commands, 101 (0x65) and 102 (0x66)
-        101 - sending 8 bytes
-        102 - sending 16 bytes
-
-    */
-    data := make([]byte, 8)
-    data[0] = 0x65   // Major command
-    data[1] = 0x0C   // Minor command
-    data[2] = color  // Data LSB
-    data[3] = 0x00   // Data MSB
-    data[4] = 0x00   // DataHID[0]
-    data[5] = 0x00   // DataHID[1]
-    data[6] = 0x00   // DataHID[2]
-    data[7] = 0x00   // DataHID[3]
-
-    bytesWritten, err := leds.SendFeatureReport(data)
-    if err != nil {
-        log.Fatalf("Could not write bytes to delcom LED action. %s\n", err)
-    }
-
-    if bytesWritten == -1 {
-        log.Fatalf("No bytes written")
-    }
+    sendCommand(command{color: color})
 }
 
 func Off(color byte) {
+    // turn off flash
+    sendCommand(command{color: color, shutdown: true, flash: true}) 
+    // then turn off the light
+    sendCommand(command{color: color, shutdown:true})
+}
+
+
+func sendCommand(comm command) {
     leds, err := hid.Open(vendorId, productId, "")
     if err != nil {
         log.Fatalf("Could not open leds device: %s", err)
     }
-    log.Println("Opened connection")
     defer leds.Close()
 
     /*
@@ -118,20 +57,37 @@ func Off(color byte) {
     */
     data := make([]byte, 8)
     data[0] = 0x65   // Major command
-    data[1] = 0x0C   // Minor command
-    data[2] = 0x00  // Data LSB
-    data[3] = color   // Data MSB
+    if comm.flash {
+        data[1] = 0x14 // Minor command
+    } else {
+        data[1] = 0x0C // Minor command
+    }
+    
+    if comm.shutdown {
+        if comm.flash {
+            data[2] = comm.color  // Data LSB
+            data[3] = 0x00   // Data MSB 
+        } else {
+            data[2] = 0x00  // Data LSB
+            data[3] = comm.color // Data MSB 
+        }        
+    } else {
+        if comm.flash {
+            data[2] = 0x00  // Data LSB
+            data[3] = comm.color // Data MSB             
+        } else {
+            data[2] = comm.color  // Data LSB
+            data[3] = 0x00   // Data MSB 
+        }   
+    }
+
     data[4] = 0x00   // DataHID[0]
     data[5] = 0x00   // DataHID[1]
     data[6] = 0x00   // DataHID[2]
     data[7] = 0x00   // DataHID[3]
 
-    bytesWritten, err := leds.SendFeatureReport(data)
+    _, err = leds.SendFeatureReport(data)
     if err != nil {
-        log.Fatalf("Could not send feature report to do dummy action. %s\n", err)
-    }
-
-    if bytesWritten == -1 {
-        log.Fatalf("No bytes written")
+        log.Fatalf("Could not write bytes to delcom LED action. %s\n", err)
     }
 }
